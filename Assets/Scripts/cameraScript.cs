38df8d0f739558082a95ce2385494367893bbbc7
Luke Schwartz <luke@texasschwartz.com>
2020-10-23 22:07:34 -0400

    Added Player Movement Script

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public Rigidbody player;
+    //public float scrollDistance;
+    //public float scrollSpeed = 0.1f;
+
+    private void Start()
+    {
+        //scrollDistance = Camera.main.transform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        Camera.main.transform.position = new Vector3(player.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        //Debug.Log(player.transform.position.x);
+        //Camera.main.transform.position = new Vector3(scrollDistance, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        //scrollDistance += scrollSpeed;
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public int speed = 5;
+    public Rigidbody player;
+    public float jumpheight = 7;
+
+    private void FixedUpdate()
+    {
+        player.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, player.velocity.y, player.velocity.z);
+
+        if (player.velocity.x > 0)
+        {
+            gameObject.transform.rotation = Quaternion.LookRotation(Vector3.right);
+        }
+        if (player.velocity.x < 0)
+        {
+            gameObject.transform.rotation = Quaternion.LookRotation(-Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.W) && GroundCheck())
+        {
+            player.velocity = new Vector3(player.velocity.x, jumpheight, player.velocity.z);
+        }
+
+        
+    }
+
+    private bool GroundCheck()
+    {
+        float distance = 1.1f;
+        Vector3 dir = new Vector3(0, -1, 0);
+        return Physics.Raycast(player.position, dir, distance);
+    }
+}
