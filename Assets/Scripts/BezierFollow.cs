4a81d79006f8861738341cb5a93ca0b99b6fb2f9
Prismly <prismly55@gmail.com>
2020-10-24 17:58:26 -0400

    Added basic movement, a dummy enemy, and a melee attack

---
+++ Assets/Scripts/BezierFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector2 bombPosition;
+
+    private float speedModifier;
+
+    private bool coroutineAllowed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    //Decrements this enemy's health by the specified integer value
+    //Returns 'true' if this call results in this enemy's death, for melee speed boost purposes
+    public bool TakeDamage(int damageReceived)
+    {
+        if (gameObject != null)
+        {
+            health -= damageReceived;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        Debug.Log("An enemy that does not exist just tried to TakeDamage().");
+        return false;
+    }
+}
