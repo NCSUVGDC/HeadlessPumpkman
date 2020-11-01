c82cc286d6393822720a0d901f5f3bfbf40952d6
Nico <nico@hbcafe.com>
2020-11-01 15:52:25 -0500

    Basic start menu functionality

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void enterCampaign()
+    {
+        SceneManager.LoadScene("Level1_Joseph");
+    }
+
+    public void enterEndless()
+    {
+        SceneManager.LoadScene("NicoScene");
+    }
+
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void quitGame()
+    {
+        //Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
